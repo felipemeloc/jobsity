4690ac68aeb77bfc65225b51af5f2392f0f6c28b
“Felipe <felipemeloc@gmail.com>
2021-07-29 23:55:45 -0500

    [fix] The main file was deleted

--- main.py
+++
@@ -1,7 +0,0 @@
-import sys
-import os
-
-if __name__ == "__main__":
-    sys.path.append('/Users/felipemeloc/Documents/jobsity_challenge/jobsity/ingest_data')
-    from ingest_data import ingest_data
-    ingest_data.main_ingest()
